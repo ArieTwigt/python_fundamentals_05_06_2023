feb35a80b142f3987ff1ca34990247d23967ae9a
Arie Twigt <ariekanarie@twigtd4.nl>
2023-06-06 11:41:00 +0200

    added assignments for chapter 5

--- assignments_5.py
+++ assignments_5.py
@@ -10,4 +10,31 @@ if first_letter == "a":
     print("Your name starts with an 'A'")
 else:
     print("Your name does not start with an 'A'")
+
+
+# Assignment 2 - Create a conditional statement that indicates if your name begins with a vowel.
+# 
+# %%
+
+name = "Arie"
+vowels = ['a', 'e', 'o', 'u', 'i', 'y']
+
+first_letter = name[0].lower()
+
+#%%
+if first_letter in vowels:
+    new_name = name.replace(name[0], "B")
+else:
+    new_name = name.replace(name[0], "A")
+
+print(new_name)
+# Assignment 3: Create a conditional statement that indicates if your age is between 18 and 68.
+# %%
+my_age = 40
+
+if my_age > 18 and my_age < 68:
+    print("Between 18 and 68")
+# %%
+if 18 < my_age < 68:
+    print("Between 18 and 68")
 # %%
