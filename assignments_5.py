bbde35608b1fe2d20a56529f26c6f991457cb02b
Arie Twigt <ariekanarie@twigtd4.nl>
2023-06-06 11:29:50 +0200

    added first assignment for 5

---
+++ assignments_5.py
@@ -0,0 +1,13 @@
+# Assignment 1 - Create a conditional statement that indicates if your name starts with an "A or not
+#%%
+name = "Arie"
+
+first_letter = name[0].lower()
+
+
+# %%
+if first_letter == "a":
+    print("Your name starts with an 'A'")
+else:
+    print("Your name does not start with an 'A'")
+# %%
